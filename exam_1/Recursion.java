eb96af1cd24102df0ba24f5cc47b8b1330e95564
Ben Christian Aceveda <101437225+bnchrstn@users.noreply.github.com>
2023-01-31 17:47:11 +0800

    Update Recursion.java

--- exam_1/Recursion.java
+++ exam_1/Recursion.java
@@ -8,18 +8,18 @@ public class App {
     }
 
     static void removeLetters(String str) {
-        String result = removeConsonants(str, "", 0);
+        String result = removeLetters(str, "", 0);
         System.out.println(result);
     }
 
-    static String removeConsonants(String fullName, String result, int index) {
+    static String removeLetters(String fullName, String result, int index) {
         if (index >= fullName.length()) {
           return result;
         }
         char letter = fullName.charAt(index);
         String vowels = "aeiou";
         int isVowel = vowels.indexOf(letter) >= 0 ? 0 : 1;
-        return removeConsonants(fullName, isVowel == 1 ? result + letter : result, index + 1);
+        return removeLetters(fullName, isVowel == 1 ? result + letter : result, index + 1);
     }
 
 }
